85a07f48a8661875001a195e189dd1671b168240
matheussvini <matheussvini@outlook.com>
2022-10-06 13:33:20 -0300

    CPF number validation and formatting

--- src/components/SectionPage.js
+++ src/components/SectionPage.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import styled from "styled-components";
+import alfabeto from "./alfabeto";
 
 function isEmpty(obj) {
   for (let prop in obj) {
@@ -15,6 +16,8 @@ export default function SectionPage() {
   const { sessaoID } = useParams();
   const [error, setError] = useState(null);
   const [seatsSelected, setSeatsSelected] = useState([]);
+  const [nameUser, setNameUser] = useState(undefined);
+  const [cpfUser, setCpfUser] = useState("");
 
   useEffect(() => {
     const promise = axios.get(
@@ -37,6 +40,46 @@ export default function SectionPage() {
 
   if (error === null && isEmpty(section) === true) {
     return <div>Carregando...</div>;
+  }
+
+  function name(e) {
+    const value = e.target.value;
+  }
+
+  function cpf(e) {
+    const value = e.target.value;
+
+    let cpfUpdated;
+    if (value !== "") {
+      for (let i = 0; i < value.length; i++) {
+        if (alfabeto.includes(value[i])) {
+          return setCpfUser(""), alert("CPF inválido");
+        }
+      }
+      cpfUpdated = value.replace(
+        /(\d{3})(\d{3})(\d{3})(\d{2})/,
+        function (regex, argumento1, argumento2, argumento3, argumento4) {
+          return (
+            argumento1 + "." + argumento2 + "." + argumento3 + "-" + argumento4
+          );
+        }
+      );
+      setCpfUser(cpfUpdated);
+      let regex = /\d/g;
+      let cpfOnlyNumber = cpfUpdated.match(regex).join("");
+      console.log("filtro", cpfOnlyNumber);
+
+      let cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}))$/;
+      if (
+        cpfValido.test(cpfUpdated) == false &&
+        (cpfUpdated.length === 14 ||
+          cpfOnlyNumber.length === 11 ||
+          cpfOnlyNumber.length === 0)
+      ) {
+        alert("CPF invalido");
+        setCpfUser("");
+      }
+    }
   }
 
   return (
@@ -75,6 +118,38 @@ export default function SectionPage() {
           <span>Indisponível</span>
         </div>
       </Subtitle>
+      <Formulario>
+        <h4>Nome do comprador:</h4>
+        <input
+          type="text"
+          placeholder="Digite seu nome..."
+          onChange={(e) => name(e)}
+          value={nameUser}
+        />
+        <h4>CPF do comprador:</h4>
+        <input
+          type="text"
+          placeholder="Digite seu CPF..."
+          onChange={(e) => cpf(e)}
+          value={cpfUser}
+          maxLength="14"
+          pattern="[0-9]"
+        />
+      </Formulario>
+
+      <Footer>
+        <BoxMovie>
+          <img
+            src={section.movie.posterURL}
+            alt={`Capa do filme ${section.movie.title}`}
+          />
+        </BoxMovie>
+        <h1>
+          {section.movie.title}
+          <br />
+          {section.day.weekday} - {section.day.date}
+        </h1>
+      </Footer>
     </Container>
   );
 }
@@ -85,6 +160,7 @@ const Container = styled.div`
   flex-direction: column;
   width: 100%;
   max-width: 390px;
+  height: 100vh;
   margin: 0 15px;
   box-sizing: border-box;
   padding: 0 20px;
@@ -98,8 +174,8 @@ const Container = styled.div`
     justify-content: center;
   }
   h4 {
-    font-size: 20px;
-    line-height: 23px;
+    font-size: 18px;
+    line-height: 21px;
     color: #293845;
   }
 `;
@@ -144,4 +220,47 @@ const Subtitle = styled.div`
     line-height: 15px;
     color: #4e5a65;
   }
+`;
+const Footer = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  //width: 100%;
+  height: 117px;
+  width: 390px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  transform: translate(-48%, 0);
+  background-color: #dfe6ed;
+  box-sizing: border-box;
+  border: 1px solid #9eadba;
+  overflow: hidden;
+  h1 {
+    font-size: 26px;
+    line-height: 30px;
+    color: #293845;
+  }
+`;
+const BoxMovie = styled.div`
+  height: 89px;
+  width: 64px;
+  border-radius: 2px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  //margin: 0 15px 15px 11px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 14px;
+
+  img {
+    width: 48px;
+    height: 72px;
+  }
+`;
+const Formulario = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: aquamarine;
 `;
